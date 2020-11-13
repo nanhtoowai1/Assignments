a2d31963b8f879f800796abc894754e49192506d
nanhtoowai1 <nanhtoowai1@gmail.com>
2020-11-13 13:07:49 +0630

    Assignment9

---
+++ Assignment9.java
@@ -0,0 +1,30 @@
+class Frog{
+	private int depth,jumpdepth,slipdepth;
+	public int trydepth=0;
+	public int count=0;
+	
+	public void jump(int depth,int jumpdepth,int slipdepth) {
+		this.depth=depth;
+		this.jumpdepth=jumpdepth;
+		this.slipdepth=slipdepth;
+		
+		trydepth=this.jumpdepth-this.slipdepth;
+		while(this.depth>0 && trydepth>0) {
+			this.depth-=trydepth;
+			count++;
+		}
+		
+	}
+	
+	public int totaljump() {
+		return count;
+	}
+	
+}
+public class Assignment9 {
+	public static void main(String[] args) {
+		Frog frog=new Frog();
+		frog.jump(30,5,2);
+		System.out.println("Frog jumps "+frog.totaljump()+" times");
+	}
+}
